18eb5b8cdd47dca695b4417f37866269db505aa3
LiyuanWang123 <liyuan@umail.ucsb.edu>
2019-04-02 11:32:03 -0700

    Update variables.tf

--- Jenkins/variables.tf
+++ Jenkins/variables.tf
@@ -1,8 +1,6 @@
 variable "access_key" {
-	default = "AKIAIQXXFEH3KSRE64RA"
 }
 variable "secret_key" {
-	default = "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"
 }
 variable "region" {
   description = "AWS region to launch servers."
