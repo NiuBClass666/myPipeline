2f62044943289b99ebe6c298440d0442bb88e45a
LiyuanWang123 <liyuan@umail.ucsb.edu>
2019-04-19 15:05:14 -0700

    simplify Java installation

--- Jenkins/local-exec2.sh
+++ Jenkins/local-exec2.sh
@@ -5,12 +5,10 @@ apt-get update
 wget --no-check-certificate -qO - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add - 
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list 
 apt-get install -y python-software-properties debconf-utils
-add-apt-repository ppa:webupd8team/java --yes
-sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-echo "CONFIG DONE" 
-apt-get install oracle-java8-installer -y
+
+apt update
+apt install openjdk-8-jdk
+
 
 apt-get update
 apt install apt-transport-https ca-certificates curl software-properties-common -y
